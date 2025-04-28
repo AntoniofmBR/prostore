69e4ac42fd5065048f1295952d6b18db25b0b9a4
Antonio Fonseca <antonio.fonse06@gmail.com>
2025-04-28 17:32:50 -0300

    Finish user orders page and profile page

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,7 +1,26 @@
-export default function ProfilePage() {
+import { Metadata } from 'next'
+import { SessionProvider } from 'next-auth/react'
+
+import { auth } from '@/auth'
+import ProfileForm from './profile-form'
+
+export const metadata: Metadata = {
+  title: 'Customer Profile',
+}
+
+export default async function ProfilePage() {
+  const session = await auth()
+
   return (
-    <>
-      Profile
-    </>
+    <SessionProvider
+      session={ session }
+    >
+      <div className="max-w-md mx-auto space-y-4">
+        <h2 className="h2-bold">
+          Profile
+        </h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
   )
 }
